7b5126b72902647112633429ed308198aa54eab7
artstalker <groshevoy@gmail.com>
2013-06-05 18:26:55 +0300

    Nav finihsed

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -19,12 +19,20 @@ namespace SportsStore.WebUI.Controllers
 			 this.repository = productRepository;
 		 }
 
-	    public ViewResult List(int page = 1)
+	    public ViewResult List(string category, int page = 1)
 	    {
 		    ProductsListViewModel model = new ProductsListViewModel
 			    {
-				    Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
-				    PagingInfo = new PagingInfo { CurrentPage = page, ItemsPerPage = PageSize, TotalItems = repository.Products.Count() }
+				    Products =
+					    repository.Products.Where(p => category == null || p.Category == category).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+				    PagingInfo =
+					    new PagingInfo
+						    {
+							    CurrentPage = page,
+							    ItemsPerPage = PageSize,
+							    TotalItems = category == null ? repository.Products.Count() : repository.Products.Count(e => e.Category == category)
+						    },
+				    CurrentCategory = category
 			    };
 		    return View(model);
 	    }
